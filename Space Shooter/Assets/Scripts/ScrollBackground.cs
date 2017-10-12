84a16572a0bf3896c4ecf8f0cbc9bc6a5d3466c9
AntoniusMaximus80 <toni.mutanen@cs.tamk.fi>
2017-10-12 14:07:54 +0300

    Scrolling background.

---
+++ Space Shooter/Assets/Scripts/ScrollBackground.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBackground : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _firstBackground;
+    
+    [SerializeField]
+    private GameObject _secondBackground;
+
+    private float _distanceBetweenBackgrounds;
+
+    // Use this for initialization
+    void Start () {
+        _distanceBetweenBackgrounds = _firstBackground.transform.position.y -
+            _secondBackground.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _firstBackground.transform.Translate(Vector3.down * Time.deltaTime * 4f);
+        _secondBackground.transform.Translate(Vector3.down * Time.deltaTime * 4f);
+        if (_firstBackground.transform.position.y <= -32f)
+        {
+            _firstBackground.transform.Translate(0f, _firstBackground.transform.position.y + _distanceBetweenBackgrounds, 0f);
+        }
+        if (_secondBackground.transform.position.y <= -32f)
+        {
+            _secondBackground.transform.Translate(0f, _secondBackground.transform.position.y + _distanceBetweenBackgrounds, 0f);
+        }
+    }
+}
